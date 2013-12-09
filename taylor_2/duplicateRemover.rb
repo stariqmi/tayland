43eea1b6e6241815edb68f2ab40cd419142dfe89
Salman Tariq Mirza <stariqmi@gmail.com>
2013-12-08 20:54:46 -0800

    additions made

--- taylor_2/duplicateRemover.rb
+++ taylor_2/duplicateRemover.rb
@@ -4,7 +4,7 @@ require 'spreadsheet'
 source_xls = Spreadsheet.open 'MLS Cash Buyers/main_properties.xls'
 source_sheet = source_xls.worksheet 0
 all_headers = source_sheet.row(0)
-headers = all_headers[0...4] << "Same" << all_headers[5] << "# Owns"
+headers = all_headers[0...4] << all_headers[5]
 
 def checkUniqueness row, unique_rows
 	unique_rows.each do |ur|
@@ -17,7 +17,7 @@ end
 unique_rows = [] 
 
 source_sheet.each 1 do |row|
-	info = row[0..4] << row[5]
+	info = row[0...4] << row[5]
 	puts "Checking '#{info[0]}' for uniqueness ... "
 	unique_rows << info if checkUniqueness info, unique_rows
 end
